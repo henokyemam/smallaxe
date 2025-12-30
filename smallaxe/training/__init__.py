1911ae5d6625693399ee1535605c79deaa1eafb8
henokyemam <henokadem1@gmail.com>
2025-12-30 12:32:01 -0800

    Add RandomForestRegressor

--- smallaxe/training/__init__.py
+++ smallaxe/training/__init__.py
@@ -1,9 +1,11 @@
 """Training module - model classes and factories."""
 
 from smallaxe.training.base import BaseClassifier, BaseModel, BaseRegressor
+from smallaxe.training.random_forest import RandomForestRegressor
 
 __all__ = [
     "BaseModel",
     "BaseRegressor",
     "BaseClassifier",
+    "RandomForestRegressor",
 ]
